c8c47f392a993c07c3fd910aefdd7ea3f0eb0f65
Mohamed A. Salah <me@msalah.net>
2024-09-24 21:01:47 +0300

    use one hash function

--- src/hashring.rs
+++ src/hashring.rs
@@ -3,8 +3,6 @@ use std::{
     hash::{DefaultHasher, Hash, Hasher},
 };
 
-use sha2::{Digest, Sha256};
-
 /// A struct representing the consistent hash ring with virtual nodes
 pub struct HashRing<T: Clone> {
     ring: BTreeMap<u64, T>, // Hash ring, where key is the hash and value is the shard ID
@@ -27,7 +25,7 @@ where
         for i in 0..self.virtual_nodes {
             // Create a virtual node by appending the index to the shard name
             let virtual_shard = format!("{}-VN{}", shard, i);
-            let hash = hash_shard(&virtual_shard);
+            let hash = Self::hash(&virtual_shard);
             println!("Adding shard: {}:{}", virtual_shard, hash);
             self.ring.insert(hash, value.clone());
         }
@@ -35,7 +33,7 @@ where
 
     /// Get the physical shard corresponding to the given key (short ID)
     pub fn get_shard(&self, key: &str) -> T {
-        let hash = hash_str(key);
+        let hash = Self::hash(key);
         println!("Retrieving pool from shard: {}", hash);
         // Find the first shard hash >= hashed key
         let shard = self
@@ -46,19 +44,11 @@ where
             .1;
         shard.to_owned()
     }
-}
 
-/// Hashes a shard (or virtual node) and returns the hash as u64
-fn hash_shard(shard: &str) -> u64 {
-    let mut hasher = DefaultHasher::new();
-    shard.hash(&mut hasher);
-    hasher.finish()
-}
-
-/// Hashes a string (typically the short ID) and returns the hash as u64
-fn hash_str(s: &str) -> u64 {
-    let mut hasher = Sha256::new();
-    hasher.update(s);
-    let result = hasher.finalize();
-    u64::from_be_bytes(result[0..8].try_into().unwrap()) // Return the first 8 bytes as u64
+    /// Hashes a shard (or virtual node) and returns the hash as u64
+    fn hash(shard: &str) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        shard.hash(&mut hasher);
+        hasher.finish()
+    }
 }
